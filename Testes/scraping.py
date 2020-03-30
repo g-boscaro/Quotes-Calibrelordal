a047ac46758730e0f3da58c891e0d1e360f62815
umFido <54111381+umFido@users.noreply.github.com>
2020-03-30 12:46:28 -0400

    Update scraping.py
    O conteudo da pagina esta em texto preformatado <pre>, o que facilita pegar o texto

--- Testes/scraping.py
+++ Testes/scraping.py
@@ -1,7 +1,15 @@
-import bs4 as bs
 import requests
+from bs4 import BeautifulSoup
 
+#Link da pagina
 quotes_url = "https://twitch.center/customapi/quote/list?token=25693f14"
 
+#Request da pagina
 pagina = requests.get(quotes_url)
-print(pagina.text[0:500])
+#Conteúdo da pagina em uma grande string
+texto = pagina.text
+
+#Cria uma lista com todas as frases separadas pelo delimitador de linha \n
+frases = texto.splitlines()
+#print(frases)
+print(len(frases), "quotes.")
