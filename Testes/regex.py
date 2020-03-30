0075b7f18e584149a6ec134c0c7ea238f41c4c01
umFido <54111381+umFido@users.noreply.github.com>
2020-03-30 11:53:44 -0400

    Update de comentários

--- Testes/regex.py
+++ Testes/regex.py
@@ -4,17 +4,17 @@
 import re
 
 #String de teste
-quote = r"""15. "Meu Deus, eu encostei numa menina, mano" -Saga"""
+quote = r'15. "Meu Deus, eu encostei numa menina, mano" -Saga'
 
 #A expressão captura um grupo(), define um caracter no range de 0 a 9 [0-9], e faz o match de um a infinitos caracteres +
-#group() define qual o grupo achado será utilizado
+#.group() define qual o grupo achado será utilizado
 num_quote = re.search(r'([0-9]+)', quote).group()
 #print(num_quote)
 print("Numero do quote:", num_quote)
 
-find_quote = re.search(r'"([^\"]*)"', quote).group(1)
-#print(find_quote)
-print("Quote:", find_quote)
+texto_quote = re.search(r'"([^\"]*)"', quote).group(1)
+#print(texto_quote)
+print("Quote:", texto_quote)
 
 #autor_quote = re.search(r'([^-]\w+$)', quote)
 autor_quote = re.search(r'([^-]\w+$)', quote).group()
